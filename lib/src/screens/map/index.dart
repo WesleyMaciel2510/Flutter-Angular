1b30ddd87a71c761d52fc6941576c298993919e7
Wesley Maciel <wesley.2510@hotmail.com>
2024-08-07 12:50:36 -0300

    adding Image to MapScreen

--- lib/src/screens/map/index.dart
+++ lib/src/screens/map/index.dart
@@ -9,13 +9,24 @@ class MapScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Map Screen'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/profile');
-          },
-          child: const Text('Go to Profile'),
-        ),
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 1 / 2,
+            child: Image.asset(
+              'assets/mapscreen_header.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 12.0),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/profile');
+            },
+            child: const Text('Go to Profile'),
+          ),
+        ],
       ),
     );
   }
